b932feaf647b8877651fe444c9b1e1faeaadc49f
Muhammad Afaque Khan <mafaqkhan@muhammads-air.localdomain>
2023-07-21 23:08:29 +0200

    kubernetes manifest file for v2

--- src/main/java/com/istio/demo/userdataservice/controller/UserDataController.java
+++ src/main/java/com/istio/demo/userdataservice/controller/UserDataController.java
@@ -3,7 +3,6 @@ package com.istio.demo.userdataservice.controller;
 import com.istio.demo.userdataservice.model.UserData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +13,9 @@ public class UserDataController {
 
     @GetMapping("/user_data/{username}")
     public ResponseEntity<UserData> getUserData(@PathVariable String username) {
-        log.info("Request: username:"+username);
+
         if ("user1".equals(username)) {
-            UserData userData = UserData.builder().name("John Doe").age(30).build();
+            UserData userData = UserData.builder().name("Adam Smith").age(48).build();
             log.info("Request: userData:"+userData.toString());
             return ResponseEntity.ok(userData);
         } else {
